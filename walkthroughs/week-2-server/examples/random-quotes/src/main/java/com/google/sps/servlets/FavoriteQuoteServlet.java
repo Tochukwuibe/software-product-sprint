d5feeab504bfd4695efefd56121acdd145c0d9ac
Tochukwu Ibe-Ekeocha <tochukwu.claver@gmail.com>
2020-03-01 18:10:18 -0500

    add message sending functionality

--- walkthroughs/week-2-server/examples/random-quotes/src/main/java/com/google/sps/servlets/FavoriteQuoteServlet.java
+++ walkthroughs/week-2-server/examples/random-quotes/src/main/java/com/google/sps/servlets/FavoriteQuoteServlet.java
@@ -14,31 +14,28 @@
 
 package com.google.sps.servlets;
 
-import com.google.appengine.api.datastore.DatastoreService;
-import com.google.appengine.api.datastore.DatastoreServiceFactory;
-import com.google.appengine.api.datastore.Entity;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/** Servlet responsible for creating new tasks. */
-@WebServlet("/new-task")
-public class NewTaskServlet extends HttpServlet {
+/** Servlet that returns a random quote. */
+@WebServlet("/favorite-quote")
+public final class FavoriteQuoteServlet extends HttpServlet {
+
 
   @Override
-  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    String title = request.getParameter("title");
-    long timestamp = System.currentTimeMillis();
-    
-    Entity taskEntity = new Entity("Task");
-    taskEntity.setProperty("title", title);
-    taskEntity.setProperty("timestamp", timestamp);
+  public void init() {
+   
+  }
 
-    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-    datastore.put(taskEntity);
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
-    response.sendRedirect("/index.html");
+    response.setContentType("text/html;");
+    response.getWriter().println("Dread it, run from it, destiny arives all the same. - Big purple guy");
   }
 }
